6cd3725b7bf3a781124f95b1f9f407e46d5a44c7
petershine <petershine@fxceed.com>
2024-06-09 10:30:48 -0400

    deactivate until actually needed

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		.package(url: "https://github.com/petershine/fXDKit", branch: "master"),
-		.package(url: "https://github.com/tayloraswift/swift-png", from: "4.4.0"),
+//		.package(url: "https://github.com/tayloraswift/swift-png", branch: "master"),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
