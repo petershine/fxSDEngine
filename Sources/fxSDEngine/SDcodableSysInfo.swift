f42d6218804b83ef4b49778f11336399fb6528a9
petershine <petershine@fxceed.com>
2024-06-23 18:43:08 -0400

    [reduce] currently unnecessary variables

--- Sources/fxSDEngine/SDcodableSysInfo.swift
+++ Sources/fxSDEngine/SDcodableSysInfo.swift
@@ -9,22 +9,7 @@ public struct SDcodableSysInfo: Codable {
 	var Config: SDcodableConfig? = nil
 	struct SDcodableConfig: Codable {
 		var outdir_samples: String? = nil
-		var SCUNET_tile: Int? = nil
-		var SCUNET_tile_overlap: Int? = nil
-		var SWIN_tile: Int? = nil
-		var SWIN_tile_overlap: Int? = nil
-		var SWIN_torch_compile: Bool? = nil
-		var hypertile_enable_unet: Bool? = nil
-		var hypertile_enable_unet_secondpass: Bool? = nil
-		var hypertile_enable_vae: Bool? = nil
-		var hypertile_max_depth_unet: Int? = nil
-		var hypertile_max_depth_vae: Int? = nil
-		var hypertile_max_tile_unet: Int? = nil
-		var hypertile_max_tile_vae: Int? = nil
-		var hypertile_swap_size_unet: Int? = nil
-		var hypertile_swap_size_vae: Int? = nil
-		var ldsr_cached: Bool? = nil
-		var ldsr_steps: Int? = nil
+		
 		var sd_checkpoint_hash: String? = nil
 		var sd_model_checkpoint: String? = nil
 	}
