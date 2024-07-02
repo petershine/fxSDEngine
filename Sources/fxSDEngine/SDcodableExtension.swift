4952d3256b5a09991cdd6adb19ec5323d0d2cbe9
petershine <petershine@fxceed.com>
2024-07-02 00:01:11 -0400

    [revise] for default instance

--- Sources/fxSDEngine/SDcodableExtension.swift
+++ Sources/fxSDEngine/SDcodableExtension.swift
@@ -50,40 +50,40 @@ public struct SDextensionADetailer: Codable {
 	var ad_mask_k_largest: Int
 	var ad_model: String
 
-	var ad_cfg_scale: Int? = 7
-	var ad_checkpoint: String? = "Use same checkpoint"
-	var ad_clip_skip: Int? = 1
-	var ad_controlnet_guidance_end: Int? = 1
-	var ad_controlnet_guidance_start: Int? = 0
-	var ad_controlnet_model: String? = "None"
-	var ad_controlnet_module: String? = "None"
-	var ad_controlnet_weight: Int? = 1
-	var ad_inpaint_height: Int? = 512
-	var ad_inpaint_width: Int? = 512
-	var ad_mask_max_ratio: Int? = 1
-	var ad_mask_merge_invert: String? = "None"
-	var ad_mask_min_ratio: Int? = 0
-	var ad_model_classes: String? = ""
-	var ad_negative_prompt: String? = ""
-	var ad_noise_multiplier: Int? = 1
-	var ad_prompt: String? = ""
-	var ad_restore_face: Bool? = false
-	var ad_sampler: String? = "Use same sampler"
-	var ad_scheduler: String? = "Use same scheduler"
-	var ad_steps: Int? = 28
-	var ad_tab_enable: Bool? = true
-	var ad_use_cfg_scale: Bool? = false
-	var ad_use_checkpoint: Bool? = false
-	var ad_use_clip_skip: Bool? = false
-	var ad_use_inpaint_width_height: Bool? = false
-	var ad_use_noise_multiplier: Bool? = false
-	var ad_use_sampler: Bool? = false
-	var ad_use_steps: Bool? = false
-	var ad_use_vae: Bool? = false
-	var ad_vae: String? = "Use same VAE"
-	var ad_x_offset: Int? = 0
-	var ad_y_offset: Int? = 0
-	var is_api: Array<Bool?>? = []
+	var ad_cfg_scale: Int
+	var ad_checkpoint: String
+	var ad_clip_skip: Int
+	var ad_controlnet_guidance_end: Int
+	var ad_controlnet_guidance_start: Int
+	var ad_controlnet_model: String
+	var ad_controlnet_module: String
+	var ad_controlnet_weight: Int
+	var ad_inpaint_height: Int
+	var ad_inpaint_width: Int
+	var ad_mask_max_ratio: Int
+	var ad_mask_merge_invert: String
+	var ad_mask_min_ratio: Int
+	var ad_model_classes: String
+	var ad_negative_prompt: String
+	var ad_noise_multiplier: Int
+	var ad_prompt: String
+	var ad_restore_face: Bool
+	var ad_sampler: String
+	var ad_scheduler: String
+	var ad_steps: Int
+	var ad_tab_enable: Bool
+	var ad_use_cfg_scale: Bool
+	var ad_use_checkpoint: Bool
+	var ad_use_clip_skip: Bool
+	var ad_use_inpaint_width_height: Bool
+	var ad_use_noise_multiplier: Bool
+	var ad_use_sampler: Bool
+	var ad_use_steps: Bool
+	var ad_use_vae: Bool
+	var ad_vae: String
+	var ad_x_offset: Int
+	var ad_y_offset: Int
+	var is_api: Array<Bool?>
 
 	public init(from decoder: any Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -97,40 +97,40 @@ public struct SDextensionADetailer: Codable {
 		self.ad_mask_k_largest = try container.decodeIfPresent(Int.self, forKey: .ad_mask_k_largest) ?? 2
 		self.ad_model = try container.decodeIfPresent(String.self, forKey: .ad_model) ?? "face_yolov8n.pt"
 
-		self.ad_cfg_scale = try container.decodeIfPresent(Int.self, forKey: .ad_cfg_scale)
-		self.ad_checkpoint = try container.decodeIfPresent(String.self, forKey: .ad_checkpoint)
-		self.ad_clip_skip = try container.decodeIfPresent(Int.self, forKey: .ad_clip_skip)
-		self.ad_controlnet_guidance_end = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_guidance_end)
-		self.ad_controlnet_guidance_start = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_guidance_start)
-		self.ad_controlnet_model = try container.decodeIfPresent(String.self, forKey: .ad_controlnet_model)
-		self.ad_controlnet_module = try container.decodeIfPresent(String.self, forKey: .ad_controlnet_module)
-		self.ad_controlnet_weight = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_weight)
-		self.ad_inpaint_height = try container.decodeIfPresent(Int.self, forKey: .ad_inpaint_height)
-		self.ad_inpaint_width = try container.decodeIfPresent(Int.self, forKey: .ad_inpaint_width)
-		self.ad_mask_max_ratio = try container.decodeIfPresent(Int.self, forKey: .ad_mask_max_ratio)
-		self.ad_mask_merge_invert = try container.decodeIfPresent(String.self, forKey: .ad_mask_merge_invert)
-		self.ad_mask_min_ratio = try container.decodeIfPresent(Int.self, forKey: .ad_mask_min_ratio)
-		self.ad_model_classes = try container.decodeIfPresent(String.self, forKey: .ad_model_classes)
-		self.ad_negative_prompt = try container.decodeIfPresent(String.self, forKey: .ad_negative_prompt)
-		self.ad_noise_multiplier = try container.decodeIfPresent(Int.self, forKey: .ad_noise_multiplier)
-		self.ad_prompt = try container.decodeIfPresent(String.self, forKey: .ad_prompt)
-		self.ad_restore_face = try container.decodeIfPresent(Bool.self, forKey: .ad_restore_face)
-		self.ad_sampler = try container.decodeIfPresent(String.self, forKey: .ad_sampler)
-		self.ad_scheduler = try container.decodeIfPresent(String.self, forKey: .ad_scheduler)
-		self.ad_steps = try container.decodeIfPresent(Int.self, forKey: .ad_steps)
-		self.ad_tab_enable = try container.decodeIfPresent(Bool.self, forKey: .ad_tab_enable)
-		self.ad_use_cfg_scale = try container.decodeIfPresent(Bool.self, forKey: .ad_use_cfg_scale)
-		self.ad_use_checkpoint = try container.decodeIfPresent(Bool.self, forKey: .ad_use_checkpoint)
-		self.ad_use_clip_skip = try container.decodeIfPresent(Bool.self, forKey: .ad_use_clip_skip)
-		self.ad_use_inpaint_width_height = try container.decodeIfPresent(Bool.self, forKey: .ad_use_inpaint_width_height)
-		self.ad_use_noise_multiplier = try container.decodeIfPresent(Bool.self, forKey: .ad_use_noise_multiplier)
-		self.ad_use_sampler = try container.decodeIfPresent(Bool.self, forKey: .ad_use_sampler)
-		self.ad_use_steps = try container.decodeIfPresent(Bool.self, forKey: .ad_use_steps)
-		self.ad_use_vae = try container.decodeIfPresent(Bool.self, forKey: .ad_use_vae)
-		self.ad_vae = try container.decodeIfPresent(String.self, forKey: .ad_vae)
-		self.ad_x_offset = try container.decodeIfPresent(Int.self, forKey: .ad_x_offset)
-		self.ad_y_offset = try container.decodeIfPresent(Int.self, forKey: .ad_y_offset)
-		self.is_api = try container.decodeIfPresent([Bool?].self, forKey: .is_api)
+		self.ad_cfg_scale = try container.decodeIfPresent(Int.self, forKey: .ad_cfg_scale) ?? 7
+		self.ad_checkpoint = try container.decodeIfPresent(String.self, forKey: .ad_checkpoint) ?? "Use same checkpoint"
+		self.ad_clip_skip = try container.decodeIfPresent(Int.self, forKey: .ad_clip_skip) ?? 1
+		self.ad_controlnet_guidance_end = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_guidance_end) ?? 1
+		self.ad_controlnet_guidance_start = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_guidance_start) ?? 0
+		self.ad_controlnet_model = try container.decodeIfPresent(String.self, forKey: .ad_controlnet_model) ?? "None"
+		self.ad_controlnet_module = try container.decodeIfPresent(String.self, forKey: .ad_controlnet_module) ?? "None"
+		self.ad_controlnet_weight = try container.decodeIfPresent(Int.self, forKey: .ad_controlnet_weight) ?? 1
+		self.ad_inpaint_height = try container.decodeIfPresent(Int.self, forKey: .ad_inpaint_height) ?? 512
+		self.ad_inpaint_width = try container.decodeIfPresent(Int.self, forKey: .ad_inpaint_width) ?? 512
+		self.ad_mask_max_ratio = try container.decodeIfPresent(Int.self, forKey: .ad_mask_max_ratio) ?? 1
+		self.ad_mask_merge_invert = try container.decodeIfPresent(String.self, forKey: .ad_mask_merge_invert) ?? "None"
+		self.ad_mask_min_ratio = try container.decodeIfPresent(Int.self, forKey: .ad_mask_min_ratio) ?? 0
+		self.ad_model_classes = try container.decodeIfPresent(String.self, forKey: .ad_model_classes) ?? ""
+		self.ad_negative_prompt = try container.decodeIfPresent(String.self, forKey: .ad_negative_prompt) ?? ""
+		self.ad_noise_multiplier = try container.decodeIfPresent(Int.self, forKey: .ad_noise_multiplier) ?? 1
+		self.ad_prompt = try container.decodeIfPresent(String.self, forKey: .ad_prompt) ?? ""
+		self.ad_restore_face = try container.decodeIfPresent(Bool.self, forKey: .ad_restore_face) ?? false
+		self.ad_sampler = try container.decodeIfPresent(String.self, forKey: .ad_sampler) ?? "Use same sampler"
+		self.ad_scheduler = try container.decodeIfPresent(String.self, forKey: .ad_scheduler) ?? "Use same scheduler"
+		self.ad_steps = try container.decodeIfPresent(Int.self, forKey: .ad_steps) ?? 28
+		self.ad_tab_enable = try container.decodeIfPresent(Bool.self, forKey: .ad_tab_enable) ?? true
+		self.ad_use_cfg_scale = try container.decodeIfPresent(Bool.self, forKey: .ad_use_cfg_scale) ?? false
+		self.ad_use_checkpoint = try container.decodeIfPresent(Bool.self, forKey: .ad_use_checkpoint) ?? false
+		self.ad_use_clip_skip = try container.decodeIfPresent(Bool.self, forKey: .ad_use_clip_skip) ?? false
+		self.ad_use_inpaint_width_height = try container.decodeIfPresent(Bool.self, forKey: .ad_use_inpaint_width_height) ?? false
+		self.ad_use_noise_multiplier = try container.decodeIfPresent(Bool.self, forKey: .ad_use_noise_multiplier) ?? false
+		self.ad_use_sampler = try container.decodeIfPresent(Bool.self, forKey: .ad_use_sampler) ?? false
+		self.ad_use_steps = try container.decodeIfPresent(Bool.self, forKey: .ad_use_steps) ?? false
+		self.ad_use_vae = try container.decodeIfPresent(Bool.self, forKey: .ad_use_vae) ?? false
+		self.ad_vae = try container.decodeIfPresent(String.self, forKey: .ad_vae) ?? "Use same VAE"
+		self.ad_x_offset = try container.decodeIfPresent(Int.self, forKey: .ad_x_offset) ?? 0
+		self.ad_y_offset = try container.decodeIfPresent(Int.self, forKey: .ad_y_offset) ?? 0
+		self.is_api = try container.decodeIfPresent([Bool?].self, forKey: .is_api) ?? []
 	}
 }
 
