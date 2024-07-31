46a3b6a9d9b18ed7f87c88e26e1ac22b08237eb9
petershine <petershine@fxceed.com>
2024-07-31 17:00:42 -0400

    open: for Monitoring

--- Sources/SDEngine/SDcodableProgress.swift
+++ Sources/SDEngine/SDcodableProgress.swift
@@ -16,11 +16,11 @@ public struct SDcodableProgress: Codable {
 public struct SDcodableState: Codable {
 	var interrupted: Bool? = nil
 	public var job: String? = nil
-	var job_count: Int? = nil
-	var job_no: Int? = nil
-	var job_timestamp: String? = nil
-	var sampling_step: Int? = nil
-	var sampling_steps: Int? = nil
+	public var job_count: Int? = nil
+	public var job_no: Int? = nil
+	public var job_timestamp: String? = nil
+	public var sampling_step: Int? = nil
+	public var sampling_steps: Int? = nil
 	var skipped: Bool? = nil
 	var stopping_generation: Bool? = nil
 }
