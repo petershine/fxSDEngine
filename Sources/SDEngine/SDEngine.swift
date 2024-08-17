c5005b72966e6a50592d45e8d58fc1a71e486388
petershine <petershine@fxceed.com>
2024-08-17 16:50:23 -0400

    reduce

--- Sources/SDEngine/SDEngine.swift
+++ Sources/SDEngine/SDEngine.swift
@@ -6,7 +6,7 @@ import UIKit
 import fXDKit
 
 
-public protocol SDEngine: Observable {
+public protocol SDEngine {
 	var mainSDNetworking: SDNetworking { get set }
 	init(mainSDNetworking: SDNetworking)
 
