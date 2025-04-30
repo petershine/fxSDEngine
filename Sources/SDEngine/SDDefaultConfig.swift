39839fb68decdbe30f088bfa4a31f74d81c015b0
petershine <petershine@fxceed.com>
2025-04-29 20:32:26 -0400

    relocated handler implementation

--- Sources/SDEngine/SDDefaultConfig.swift
+++ Sources/SDEngine/SDDefaultConfig.swift
@@ -1,19 +1,60 @@
 
 import Foundation
 import UIKit
+import Combine
+
+import fXDKit
 
 
-public enum SDDefaultConfigKey: String, CodingKey {
+public enum SDDefaultConfigKey: String, CaseIterable {
+    case hr_upscaler
     case prompt
     case negative_prompt
-    case hr_upscaler
 }
+
 
 @Observable
 open class SDDefaultConfig: @unchecked Sendable {
+    public var hr_upscaler: String? = nil
     public var prompt: String? = nil
     public var negative_prompt: String? = nil
-    public var hr_upscaler: String? = nil
 
-    public init() {}
+    public var cancellables: [AnyCancellable?] = []
+    public var handler: (([SDDefaultConfigKey: Any]?) -> Void)? = nil
+
+    public init() {
+        handler = {
+            receivedConfig in
+
+            guard let receivedConfig else {
+                return
+            }
+
+            
+            for key in SDDefaultConfigKey.allCases {
+                guard let value = receivedConfig[key] as? String,
+                      !value.isEmpty else {
+                    continue
+                }
+
+                switch key {
+                    case .hr_upscaler:
+                        self.hr_upscaler = value
+
+                    case .prompt:
+                        self.prompt = value
+
+                    case .negative_prompt:
+                        self.negative_prompt = value
+                }
+            }
+
+            fxd_log()
+            fxdPrint("hr_upscaler: \(self.hr_upscaler ?? "(not received)")")
+            fxdPrint("prompt: \(self.prompt ?? "(not received)")")
+            fxdPrint("negative_prompt: \(self.negative_prompt ?? "(not received)")")
+
+            fxdPrint("receivedConfig: \(receivedConfig)")
+        }
+    }
 }
