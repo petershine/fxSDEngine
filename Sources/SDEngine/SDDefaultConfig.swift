c52144607b30475aff328f7b9d3ce290f58f731d
petershine <petershine@fxceed.com>
2025-05-02 20:30:02 -0400

    [defaultConfig] added "allowDemoActivation"

--- Sources/SDEngine/SDDefaultConfig.swift
+++ Sources/SDEngine/SDDefaultConfig.swift
@@ -6,6 +6,8 @@ import fXDKit
 
 
 public enum SDDefaultConfigKey: String, CaseIterable {
+    case allowDemoActivation
+
     case hr_upscaler
     case prompt
     case negative_prompt
@@ -14,11 +16,22 @@ public enum SDDefaultConfigKey: String, CaseIterable {
 
 @Observable
 open class SDDefaultConfig: @unchecked Sendable {
+    public var allowDemoActivation: Bool = false
+
     public var hr_upscaler: String? = nil
     public var prompt: String? = nil
     public var negative_prompt: String? = nil
 
-    public init(hr_upscaler: String? = nil, prompt: String? = nil, negative_prompt: String? = nil) {
+    
+    public init(
+        allowDemoActivation: Bool = false,
+
+        hr_upscaler: String? = nil,
+        prompt: String? = nil,
+        negative_prompt: String? = nil
+    ) {
+        self.allowDemoActivation = allowDemoActivation
+
         self.hr_upscaler = hr_upscaler
         self.prompt = prompt
         self.negative_prompt = negative_prompt
